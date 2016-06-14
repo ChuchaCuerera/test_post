a0839c3b7db437bc2e3c5be28ef930a0b7cd8592
ChuchaCuerera <ara_altver@hotmail.com>
2016-06-13 20:50:08 -0500

    commit co formulario y base de datos

--- app/controllers/x_controller.rb
+++ app/controllers/x_controller.rb
@@ -1,4 +1,14 @@
 class XController < ApplicationController
   def index
   end
+
+  def create
+  	name = params[:name]
+  	email = params[:email]
+  	age = params[:age]
+
+  	User.create(name: name, email: email, age: age)
+
+  	redirect_to root_url
+  end
 end
